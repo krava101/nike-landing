ce8a16d60f2753398c23bf984234be3d4c2cc9ad
krava <kravalex2004@gmail.com>
2024-10-29 18:26:01 +0200

    SuperQuality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,9 @@ const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
       {label}
-      <img src={iconUrl} alt="icon" className="ml-2 w-5 h-5 rounded-full" />
+      {iconUrl && (
+        <img src={iconUrl} alt="icon" className="ml-2 w-5 h-5 rounded-full" />
+      )}
     </button>
   );
 };
--- src/components/SuperQuality.jsx
+++ src/components/SuperQuality.jsx
@@ -1,5 +1,40 @@
+import { shoe8 } from "../assets/images";
+import Button from "./Button";
+
 const SuperQuality = () => {
-  return <section className="padding">SuperQuality</section>;
+  return (
+    <section
+      id="about-us"
+      className="padding flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you <span className="text-coral-red">Super</span>{" "}
+          <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to details and your satisfaction.
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="Shoe"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
