b9e6cfbb65aa249b887e20e8111b9d8e163e33a5
krava <kravalex2004@gmail.com>
2024-10-29 19:00:55 +0200

    Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,16 @@
+import { services } from "../constants";
+import ServiceCard from "./ServiceCard";
+
 const Services = () => {
-  return <section className="padding-x py-10">Services</section>;
+  return (
+    <section className="padding-x py-10">
+      <ul className="flex flex-wrap justify-center max-container gap-9">
+        {services.map((service) => (
+          <ServiceCard key={service.label} {...service} />
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Services;
