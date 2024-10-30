1fce5ef9b2c4f3ed039ffe6532cf56a3f275687a
krava <kravalex2004@gmail.com>
2024-10-30 17:38:30 +0200

    Reviews section

--- src/components/CustomerReviews.jsx
+++ src/components/CustomerReviews.jsx
@@ -1,5 +1,31 @@
+import { reviews } from "../constants";
+import ReviewCard from "./ReviewCard";
+
 const CustomerReviews = () => {
-  return <section className="padding bg-pale-blue">CustomerReviews</section>;
+  return (
+    <section className="padding bg-pale-blue">
+      <div className="max-container">
+        <h3 className="font-palanquin text-center text-4xl font-bold capitalize">
+          What our <span className="text-coral-red">customers</span> say?
+        </h3>
+        <p className="info-text m-auto mt-4 max-w-lg text-center">
+          Hear genuine stories from our satisfied customers about their
+          exceptional experiences with us.
+        </p>
+        <ul className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+          {reviews.map((review) => (
+            <ReviewCard
+              key={review.customerName}
+              imgURL={review.imgURL}
+              customerName={review.customerName}
+              raiting={review.rating}
+              feedback={review.feedback}
+            />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReviews;
