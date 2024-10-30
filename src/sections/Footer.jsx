4189f1113801df5d0d5713b02effded7da47c187
krava <kravalex2004@gmail.com>
2024-10-30 18:26:55 +0200

    Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,69 @@
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
+
 const Footer = () => {
-  return <footer className="padding-x padding-t pb-8 bg-black">Footer</footer>;
+  return (
+    <footer className="padding-x padding-t pb-8 bg-black">
+      <div className="max-container">
+        <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+          <div className="flex flex-col items-start">
+            <a href="/">
+              <img src={footerLogo} alt="logo" width={150} height={46} />
+            </a>
+            <p className="mt-6  text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+              Get shoes ready for the new term at your nearest Nike store. Find
+              Your perfect Size In Store. Get Rewards
+            </p>
+            <ul className="flex items-center gap-5 mt-8">
+              {socialMedia.map(({ src, alt }) => (
+                <li
+                  key={src}
+                  className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+                >
+                  <img src={src} alt={alt} width={24} height={24} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+            {footerLinks.map(({ title, links }) => (
+              <div key={title}>
+                <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
+                  {title}
+                </h4>
+                <ul>
+                  {links.map(({ name, link }) => (
+                    <li
+                      key={name}
+                      className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray transition-colors ease-in-out"
+                    >
+                      <a className="block" href={link}>
+                        {name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+          <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+            <img
+              src={copyrightSign}
+              alt="copyright"
+              width={20}
+              height={20}
+              className="rounded-full m-0"
+            />
+            <p>Copyright. All rights reserved.</p>
+          </div>
+          <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
