fdf9de632223b61094f58e0e1e1b82bf53d16c3d
krava <kravalex2004@gmail.com>
2024-10-30 16:17:23 +0200

    SpecialOffer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
+  const bgColor = backgroundColor || "bg-coral-red";
+  const bdColor = borderColor || "";
+  const txtColor = textColor || "text-white";
+
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border ${bdColor} font-montserrat text-lg leading-none ${bgColor} rounded-full ${txtColor}`}
+    >
       {label}
       {iconUrl && (
         <img src={iconUrl} alt="icon" className="ml-2 w-5 h-5 rounded-full" />
